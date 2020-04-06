6b309f6880393a1124a5d7d18305709cd2211b92
Moisés Morandini <50048585+MoisesMorandini@users.noreply.github.com>
2020-04-06 08:22:19 -0300

    Update database02.js

--- src/config/database02.js
+++ src/config/database02.js
@@ -2,11 +2,6 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_NAME,
-  // uri: process.env.DB_URI,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
